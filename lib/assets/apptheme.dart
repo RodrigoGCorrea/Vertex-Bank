e00bd774a74bb2d0fc44f8e0d04089167b6a122c
Geraldo Fada <geraldofada.neto@gmail.com>
2020-09-21 14:05:38 -0300

    new colors

--- lib/assets/apptheme.dart
+++ lib/assets/apptheme.dart
@@ -5,4 +5,6 @@ class AppTheme {
 
   static const Color appBackgroundColor = Color(0xFF030708);
   static const Color textColor = Color(0xFFE5E5E5);
+  static const Color buttonColorGreen = Color(0xFF72C156);
+  static const Color buttonColorRed = Color(0xFFED174B);
 }
