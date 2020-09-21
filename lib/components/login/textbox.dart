4fed9b58bfe4c7fe0f4931dd49c6818e2ba6751a
Geraldo Fada <geraldofada.neto@gmail.com>
2020-09-21 14:11:20 -0300

    removed padding and unecessary border from textbox

--- lib/components/login/textbox.dart
+++ lib/components/login/textbox.dart
@@ -1,42 +1,39 @@
 import 'package:flutter/material.dart';
 
 import '../../assets/apptheme.dart';
+import '../../assets/sizeconfig.dart';
 
-class Textbox extends StatelessWidget {
+class Vtx_TextBox extends StatelessWidget {
   final String text;
   final bool obscureText;
 
-  const Textbox({Key key, this.obscureText = false, this.text})
+  const Vtx_TextBox({Key key, this.obscureText = false, this.text})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-      child: SizedBox(
-        width: double.maxFinite,
-        child: TextField(
-          style: TextStyle(
+    return TextField(
+      style: TextStyle(
+        color: AppTheme.textColor,
+      ),
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.symmetric(horizontal: 15),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
             color: AppTheme.textColor,
           ),
-          obscureText: obscureText,
-          decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                borderSide: const BorderSide(
-                  color: AppTheme.textColor,
-                ),
-                borderRadius: BorderRadius.circular(25),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: const BorderSide(
-                  color: AppTheme.textColor,
-                ),
-                borderRadius: BorderRadius.circular(25),
-              ),
-              labelText: text,
-              labelStyle: TextStyle(
-                color: AppTheme.textColor,
-              )),
+          borderRadius: BorderRadius.circular(25),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: AppTheme.textColor,
+          ),
+          borderRadius: BorderRadius.circular(25),
+        ),
+        labelText: text,
+        labelStyle: TextStyle(
+          color: AppTheme.textColor,
         ),
       ),
     );
