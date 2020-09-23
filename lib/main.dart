0e3386af3b8af1889703202e9dfe17a761f2b6a7
Geraldo Fada <geraldofada.neto@gmail.com>
2020-09-23 19:44:25 -0300

    init firebase on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:vertexbank/assets/apptheme.dart';
 import 'package:vertexbank/screens/login.dart';
 import 'package:vertexbank/assets/sizeconfig.dart';
@@ -15,23 +20,42 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _firebaseInit = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return OrientationBuilder(
-          builder: (context, orientation) {
-            Vtx_SizeConfig().init(constraints, orientation);
-            return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              theme: ThemeData(
-                fontFamily: 'Roboto',
-                visualDensity: VisualDensity.adaptivePlatformDensity,
-              ),
-              home: LoginScreen(),
-            );
-          },
-        );
+    return FutureBuilder(
+      future: _firebaseInit,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          // NOTE(geraldo): create a screen for connection erro
+          print("[VTX] Firebase error!!!");
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return LayoutBuilder(
+            builder: (context, constraints) {
+              return OrientationBuilder(
+                builder: (context, orientation) {
+                  Vtx_SizeConfig().init(constraints, orientation);
+                  return MaterialApp(
+                    debugShowCheckedModeBanner: false,
+                    theme: ThemeData(
+                      fontFamily: 'Roboto',
+                      visualDensity: VisualDensity.adaptivePlatformDensity,
+                    ),
+                    home: LoginScreen(),
+                  );
+                },
+              );
+            },
+          );
+
+        }
+
+        // NOTE(geraldo): create a screen for loading
+        print("[VTX] Loading...");
+
       },
     );
   }
