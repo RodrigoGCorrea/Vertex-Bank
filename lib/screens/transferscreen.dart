281fa5827c702fa2a4e17e6d0e30389a7110d329
Rodrigo Correa <rodrigo.correa4610@gmail.com>
2020-10-05 16:37:26 -0300

    refactoring the code

--- lib/screens/transferscreen.dart
+++ lib/screens/transferscreen.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:vertexbank/assets/apptheme.dart';
 import 'package:vertexbank/assets/sizeconfig.dart';
+import 'package:vertexbank/components/transferscreen/contactlist.dart';
 import 'package:vertexbank/components/transferscreen/transferscreenappbar.dart';
 import 'package:vertexbank/components/vtx_gradient.dart';
-import 'package:vertexbank/components/vtxlistviewbox.dart';
 import 'package:vertexbank/models/Contact.dart';
 
 class TransferScreen extends StatelessWidget {
@@ -18,6 +17,24 @@ class TransferScreen extends StatelessWidget {
       ContactListItem(
         contact: Contact("FDP Corp."),
       ),
+      ContactListItem(
+        contact: Contact("Marcelin Marreta"),
+      ),
+      ContactListItem(
+        contact: Contact("Jaqueline Lasquera"),
+      ),
+      ContactListItem(
+        contact: Contact("Edivaldo Jr."),
+      ),
+      ContactListItem(
+        contact: Contact("Marcelin Marreta"),
+      ),
+      ContactListItem(
+        contact: Contact("FDP Corp."),
+      ),
+      ContactListItem(
+        contact: Contact("Edivaldo Jr."),
+      ),
     ];
     return Scaffold(
       body: Background(
@@ -29,108 +46,6 @@ class TransferScreen extends StatelessWidget {
             ContactList(contactList: contactList)
           ],
         ),
-      ),
-    );
-  }
-}
-
-class ContactList extends StatefulWidget {
-  ContactList({
-    Key key,
-    @required this.contactList,
-  }) : super(key: key);
-
-  List<Widget> contactList;
-
-  @override
-  _ContactListState createState() => _ContactListState();
-}
-
-class _ContactListState extends State<ContactList> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: AppTheme.defaultHorizontalPadding,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Paying to",
-            style: TextStyle(
-              fontSize: getProportionateScreenWidth(16),
-              color: AppTheme.textColor,
-              fontWeight: FontWeight.w100,
-            ),
-          ),
-          SizedBox(height: getProportionateScreenHeight(5)),
-          Stack(
-            children: [
-              VtxListViewBox(
-                list: widget.contactList,
-                width: getProportionateScreenWidth(285),
-                height: getProportionateScreenHeight(140),
-              ),
-              Positioned(
-                right: getProportionateScreenWidth(6),
-                top: getProportionateScreenHeight(55),
-                child: Container(
-                  width: getProportionateScreenWidth(31),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SvgPicture.asset(
-                        "assets/icons/chevron-right-solid.svg",
-                        width: getProportionateScreenWidth(13),
-                      ),
-                      SizedBox(height: getProportionateScreenHeight(5)),
-                      Text(
-                        "More",
-                        style: TextStyle(
-                          fontSize: getProportionateScreenWidth(11),
-                          color: AppTheme.textColor,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              )
-            ],
-          )
-        ],
-      ),
-    );
-  }
-}
-
-class ContactListItem extends StatelessWidget {
-  final Contact contact;
-
-  const ContactListItem({Key key, this.contact}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(bottom: getProportionateScreenHeight(22)),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(top: getProportionateScreenHeight(4)),
-            child: SvgPicture.asset(
-              "assets/icons/circle-solid.svg",
-              width: getProportionateScreenWidth(4),
-            ),
-          ),
-          SizedBox(width: getProportionateScreenWidth(6)),
-          Text(
-            "${contact.nickname}",
-            style: TextStyle(
-              fontSize: getProportionateScreenWidth(14),
-              color: AppTheme.textColor,
-              fontWeight: FontWeight.w100,
-            ),
-          )
-        ],
       ),
     );
   }
