25ac2c380793db6df59bc34a85af013d1bf5219f
Geraldo Fada <geraldofada.neto@gmail.com>
2020-11-03 16:37:07 -0300

    auth cubit added

---
+++ lib/cubit/auth/auth_cubit.dart
@@ -0,0 +1,38 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+import 'package:vertexbank/api/auth.dart';
+import 'package:vertexbank/models/user.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit({
+    @required AuthApi authApi,
+  })  : assert(authApi != null),
+        _authApi = authApi,
+        super(UnauthenticatedState(email: "", password: ""));
+
+  final AuthApi _authApi;
+
+  void passwordChanged(String password) {
+    emit(UnauthenticatedState(password: password));
+  }
+
+  void emailChanged(String email) {
+    emit(UnauthenticatedState(email: email));
+  }
+
+  Future<void> loginWithEmailPassword(String email, String password) async {
+    if (state is AuthenticatedState) return null;
+
+    try {
+      await _authApi.logInWithEmailAndPassword(
+          email: email, password: password);
+      emit(AuthenticatedState(user: _authApi.user));
+    } on Exception {
+      emit(ErrorState());
+    }
+  }
+}
---
+++ lib/cubit/auth/auth_state.dart
@@ -0,0 +1,50 @@
+h cupart of 'auth_cubit.dart';
+
+abstract class AuthState extends Equatable {
+  const AuthState();
+
+  void copyWith() {}
+}
+
+class ErrorState extends AuthState {
+  @override
+  List<Object> get props => [];
+}
+
+class UnauthenticatedState extends AuthState {
+  const UnauthenticatedState({
+    String email,
+    String password,
+  })  : this.email = email,
+        this.password = password,
+        super();
+
+  final String email;
+  final String password;
+
+  @override
+  UnauthenticatedState copyWith({
+    String email,
+    String password,
+  }) {
+    return UnauthenticatedState(
+      email: this.email ?? email,
+      password: this.password ?? password,
+    );
+  }
+
+  @override
+  List<Object> get props => [email, password];
+}
+
+class AuthenticatedState extends AuthState {
+  const AuthenticatedState({
+    Stream<User> user,
+  })  : this.user = user,
+        super();
+
+  final Stream<User> user;
+
+  @override
+  List<Object> get props => [user];
+}
