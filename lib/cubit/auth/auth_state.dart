cf6d484fbf52ea50739d501ee2ea5a9e1382c58f
Geraldo Fada <geraldofada.neto@gmail.com>
2020-11-05 00:15:46 -0300

    auth cubit refactor

--- lib/cubit/auth/auth_state.dart
+++ lib/cubit/auth/auth_state.dart
@@ -2,6 +2,9 @@ part of 'auth_cubit.dart';
 
 abstract class AuthState extends Equatable {
   const AuthState();
+
+  @override
+  List<Object> get props => [];
 }
 
 class ErrorState extends AuthState {
