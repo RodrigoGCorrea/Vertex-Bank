899c8d442de8e96f2b6347ae09efc039e184fd62
RodrigoGCorrea <rodrigogc@id.uff.br>
2020-11-17 14:09:08 -0300

    started add contact screen

---
+++ lib/screens/transfer/add_contact.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:vertexbank/components/button.dart';
+import 'package:vertexbank/components/login/textbox.dart';
+import 'package:vertexbank/config/apptheme.dart';
+import 'package:vertexbank/config/size_config.dart';
+
+class AddContact extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Background(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).requestFocus(new FocusNode()),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                HeaderAddContact(),
+                SizedBox(height: getProportionateScreenHeight(35)),
+                NicknameInput(),
+                SizedBox(height: getProportionateScreenHeight(20)),
+                ContactIdInput(),
+                SizedBox(height: getProportionateScreenHeight(50)),
+                VtxButton(
+                  text: "Finish",
+                  color: AppTheme.buttonColorGreen,
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(13),
+                ),
+                Text(
+                  "or",
+                  style: TextStyle(color: AppTheme.textColor),
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(25),
+                ),
+                InkWell(
+                  onTap: () => Navigator.pop(context),
+                  child: Text(
+                    "Cancel",
+                    style: TextStyle(
+                      color: AppTheme.textColor,
+                      fontSize: getProportionateScreenWidth(12),
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+  final Widget child;
+
+  const Background({
+    Key key,
+    @required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: VtxSizeConfig.screenWidth,
+      height: VtxSizeConfig.screenHeight,
+      color: AppTheme.appBackgroundColor,
+      child: child,
+    );
+  }
+}
+
+class HeaderAddContact extends StatelessWidget {
+  const HeaderAddContact({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: VtxSizeConfig.screenHeight * 0.1),
+      child: Container(
+        alignment: Alignment.bottomLeft,
+        padding: EdgeInsets.symmetric(horizontal: 42),
+        child: Text(
+          "Add new contact",
+          style: TextStyle(
+            fontSize: getProportionateScreenWidth(24),
+            fontWeight: FontWeight.bold,
+            color: AppTheme.textColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class NicknameInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(52)),
+      child: VtxTextBox(text: "Nickname"),
+    );
+  }
+}
+
+class ContactIdInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(52)),
+      child: VtxTextBox(text: "Contact ID"),
+    );
+  }
+}
