9e413648337fd9e144f9679bb95734f550d4e4c0
Geraldo Fada <geraldofada.neto@gmail.com>
2020-11-17 18:43:29 -0300

    getit updated

--- lib/getit.dart
+++ lib/getit.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:vertexbank/api/auth.dart';
 import 'package:vertexbank/api/money.dart';
+import 'package:vertexbank/api/transaction_list.dart';
 import 'package:vertexbank/api/transfer.dart';
 
 final getIt = GetIt.instance;
@@ -9,4 +10,5 @@ void getItSetup() {
   getIt.registerSingleton<AuthApi>(AuthApi());
   getIt.registerLazySingleton<TransferApi>(() => TransferApi());
   getIt.registerLazySingleton<MoneyApi>(() => MoneyApi());
+  getIt.registerLazySingleton<TransactionListApi>(() => TransactionListApi());
 }
