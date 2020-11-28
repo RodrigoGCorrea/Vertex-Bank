9c1ad12ff21cd247d3c36b4e13cab4ab7ff9ef60
Geraldo Fada <geraldofada.neto@gmail.com>
2020-11-27 23:12:39 -0300

    add contact action fixed

--- lib/cubit/contact/action/addcontact_action_cubit.dart
+++ lib/cubit/contact/action/addcontact_action_cubit.dart
@@ -2,24 +2,24 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
-import 'package:vertexbank/api/transfer.dart';
+import 'package:vertexbank/api/contact.dart';
 import 'package:vertexbank/models/failure.dart';
 
 part 'addcontact_action_state.dart';
 
 class AddContactActionCubit extends Cubit<AddContactActionState> {
   AddContactActionCubit({
-    @required this.transferApi,
+    @required this.contactApi,
   }) : super(AddContactActionInitial());
 
-  final TransferApi transferApi;
+  final ContactApi contactApi;
 
   void addContact(String userId, String email, String nickName) async {
     emit(AddContactActionInitial());
     try {
       emit(AddContactActionLoading());
-      final contactInfo = await transferApi.findPossibleContact(email);
-      await transferApi.addContact(
+      final contactInfo = await contactApi.findPossibleContact(email);
+      await contactApi.addContact(
         userId,
         contactInfo.id,
         nickName == ""
