f5be1414c451ffae6e72935677992a336f8dfedf
Geraldo Fada <geraldofada.neto@gmail.com>
2020-11-27 22:11:56 -0300

    Contact model changed

--- lib/models/Contact.dart
+++ lib/models/Contact.dart
@@ -1,11 +1,19 @@
 import 'package:meta/meta.dart';
 
 class Contact {
+  final String id;
   final String nickname;
-  final String userID;
+  final DateTime dateAdded;
 
   const Contact({
-    @required this.userID,
+    @required this.id,
+    @required this.dateAdded,
     this.nickname,
   });
+
+  static final dbFields = {
+    "id": "id",
+    "dateAdded": "dateAdded",
+    "nickname": "nickname",
+  };
 }
