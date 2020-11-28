149014bce6bb9968cc54b64accf07d7d651b98ff
Geraldo Fada <geraldofada.neto@gmail.com>
2020-11-27 23:11:48 -0300

    getIt contact api

--- lib/getit.dart
+++ lib/getit.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:vertexbank/api/auth.dart';
+import 'package:vertexbank/api/contact.dart';
 import 'package:vertexbank/api/e_check.dart';
 import 'package:vertexbank/api/money.dart';
 import 'package:vertexbank/api/transaction_list.dart';
@@ -11,6 +12,7 @@ final getIt = GetIt.instance;
 void getItSetup() {
   getIt.registerSingleton<AuthApi>(AuthApi());
   getIt.registerLazySingleton<TransferApi>(() => TransferApi());
+  getIt.registerLazySingleton<ContactApi>(() => ContactApi());
   getIt.registerLazySingleton<MoneyApi>(() => MoneyApi());
   getIt.registerLazySingleton<TransactionListApi>(() => TransactionListApi());
   getIt.registerLazySingleton<ECheckApi>(() => ECheckApi());
